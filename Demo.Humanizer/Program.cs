374cd4c4170d79bd499a4cacfb0c006a934e83dd
Mohammed Hoque <mhoque@signifyhealth.com>
2021-06-24 15:07:34 -0500

    Added more examples

--- Demo.Humanizer/Program.cs
+++ Demo.Humanizer/Program.cs
@@ -5,32 +5,58 @@ using Humanizer.Inflections;
 
 namespace Demo.Humanizer
 {
+    /// <summary>
+    /// ORIGINAL SOURCE:
+    /// https://github.com/Humanizr/Humanizer
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
 
-            var strings = new Func<string>[]
+            var funcs = new Func<string>[]
             {
+                #region Humanize String
                 () => "user_not_found",
                 () => "user_not_found".Humanize(),
                 () => "user_not_found".Humanize(LetterCasing.Sentence),
-
-                () => StatusCode.InProgress.Humanize(),
-
-                () => DateTime.UtcNow.AddDays(-7).Humanize(),
-                () => TimeSpan.FromMilliseconds(252000).Humanize(),
-
                 () => "Mohammed Hoque".Camelize(),
                 () => "Mohammed Hoque".Underscore(),
                 () => "Mohammed Hoque".Dasherize(),
                 () => "Mohammed Hoque".Hyphenate(),
                 () => "Mohammed Hoque".Kebaberize(),
+                // acronyms are left intact
+                () => "HTML".Humanize(),
+                // any unbroken upper case string is treated as an acronym
+                () => "HUMANIZER".Transform(To.LowerCase, To.TitleCase),
+                () => "Long text to truncate".Truncate(15, "..."),
+                #endregion Humanize String
 
-                () => 1.ToWords()
+                () => StatusCode.InProgress.Humanize(),
+
+                #region DateTime
+                () => DateTime.UtcNow.AddDays(-7).Humanize(),
+                () => DateTime.Today.AtNoon().Humanize(),
+                () => DateTime.Today.AtMidnight().Humanize(),
+                () => DateTime.Today.At(4, 20, 15).Humanize(),
+                () => new DateTime(2005, 5, 5).ToOrdinalWords(),
+
+                () => TimeSpan.FromMilliseconds(252000).Humanize(),  
+
+                () => In.AprilOf(2022).Humanize(),
+                () => In.Three.Months.Humanize(),
+                () => On.July.The4th.Humanize(),
+                () => On.July.The(4).Humanize(),
+                #endregion DateTime
+
+
+                () => 10000.ToWords(),
+                () => 1.25.Billions().ToString(),
+                () => 11.ToOrdinalWords(),
+                () => 11.ToRoman(),
             };
 
-            foreach (var func in strings)
+            foreach (var func in funcs)
             {
                 Console.WriteLine(func());
             }
